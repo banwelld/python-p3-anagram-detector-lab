781d0b91d17785013259ee71f80d05ff38392b37
Dave Banwell <dave@davebanwell.com>
2024-12-21 05:40:58 -0500

    all tests passing

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,24 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word: str):
+        self.word = word
+        
+    @property
+    def word(self):
+        return self._word
+    
+    @word.setter
+    def word(self, word: str):
+        if isinstance(word, str):
+            self._word = word
+        else:
+            print("Anagram requires a string in order to evaluate its anagrams.")
+        
+    def match(self, word_list: list):
+        match_list = []
+        
+        for word in word_list:
+            if len(word) == len(self._word):
+                if sorted(word.lower()) == sorted(self.word.lower()):
+                    match_list.append(word)
+                
+        return match_list
